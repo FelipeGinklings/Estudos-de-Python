ce4f275ef5b52f565547e69891c3dcfdb23acaa4
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-04-07 18:05:31 -0300

    Criação de Personalização do Método Construtor

---
+++ Class Python/Personalizando o Método Construtor.py
@@ -0,0 +1,12 @@
+class Retangulo:
+    
+    def __init__(self, largura, altura):
+        self.l = largura
+        self.a = altura
+    
+    def area(self):
+        return self.l * self.a
+    
+r = Retangulo(7, 5)
+
+print(r.area())
